d567dd8842e8c38747726a18f642bf113d96dbb3
daniel-kav <dankavatha01@gmail.com>
2024-08-31 03:42:10 +0300

    protecting the routes

--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from .models import Notes
-from django.http import Http404
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView,DetailView, CreateView,UpdateView, DeleteView
 from .forms import NotesForm
 
 # Create your views here.
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name = 'all_notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.user.notes.all()
 
 # def notes(request):
 #     all_notes = Notes.objects.all()
@@ -33,6 +37,9 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = reverse_lazy('notes.list')
     form_class = NotesForm
+
+    def form_valid(self, form):
+        
 
 class NotesUpdateView(UpdateView):
     model = Notes
