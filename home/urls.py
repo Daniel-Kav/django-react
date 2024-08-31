d8fb8cbfb7bf03040fa41099dcdf59a52b747b50
daniel-kav <dankavatha01@gmail.com>
2024-09-01 01:05:06 +0300

    login url

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.HomeView.as_view()),
     path('auth/', views.AuthorizedView.as_view()),
+    path('login/', views.LoginInterfaceView.as_view()),
 ]
