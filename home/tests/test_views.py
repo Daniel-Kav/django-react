66a2450cbf3aa743ece016c4392abeffde02c7d2
daniel-kav <dankavatha01@gmail.com>
2024-09-09 18:10:18 +0300

    test to make sure the signup page is being served properly

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -2,3 +2,8 @@ def test_home_view(client):
     print("Testing view for home page...")
     response = client.get(path='/')
     assert response.status_code == 200
+
+def test_signup_view(client):
+    response = client.get(path='/signup/')
+    assert response.status_code == 200
+    assert 'home/register.html' in response.template_name
