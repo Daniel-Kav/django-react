bf69798b10869645c4c3e560c645dd12a7dd8201
daniel-kav <dankavatha01@gmail.com>
2024-09-09 23:39:01 +0300

    Created like posts view to like a post

--- ajax_proj/post/views.py
+++ ajax_proj/post/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Post, Like
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     return render(request, 'post/index.html',{'posts': posts})
+
+def likepost(request):
+    if request.method == 'GET':
+        post_id = request.GET['post_id']
+        likedpost = Post.objects.get(pk= post_id)
+        m = Like(post= likedpost)
+        m.save()
+        return HttpResponse("Successfully Liked")
+    else:
+        return HttpResponse("Request Method Not Allowed")
