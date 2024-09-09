45014c0e3a2e0d8d12e698c66699eaaaf7b131ad
daniel-kav <dankavatha01@gmail.com>
2024-09-09 23:30:42 +0300

    db update

--- ajax_proj/post/models.py
+++ ajax_proj/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     post_heading = models.CharField(max_length=200)
-    post_text = models.CharField()
+    post_text = models.TextField()
 
     def __str__(self):
         return self.post_heading
