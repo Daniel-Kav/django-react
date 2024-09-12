1d763b27124be4dcc6e61611541179da59e9fe49
daniel-kav <dankavatha01@gmail.com>
2024-09-12 09:47:16 +0300

    tested the list endpoint for authnticated user

--- notes/tests/test_noted_views.py
+++ notes/tests/test_noted_views.py
@@ -9,5 +9,11 @@ def test_list_endpoint_return_list(client):
     client.login(username = userr.username, password='django1234')
 
     note = Notes.objects.create(title='sample title', text='sample text', user=userr)
+    note = Notes.objects.create(title='another sample title', text='sample text', user=userr)
     response = client.get(path='/smart/notes/')
     assert response.status_code == 200
+    content = str(response.content)
+    assert 'sample title' in content
+    assert 'another sample title' in content
+    print(content)
+    assert 0 == content.count('<div>')
