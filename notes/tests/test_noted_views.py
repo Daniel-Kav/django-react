fd7ae3b9a74a5bc61f231319d59a800f9c6dcf4b
daniel-kav <dankavatha01@gmail.com>
2024-09-17 20:16:27 +0300

    using fixtures for reusable code

--- notes/tests/test_noted_views.py
+++ notes/tests/test_noted_views.py
@@ -2,14 +2,16 @@ import pytest
 from django.contrib.auth.models import User
 from notes.models import Notes
 
-
-@pytest.mark.django_db
-def test_list_endpoint_return_list(client):
+@pytest.fixture
+def logged_user(client):
     userr = User.objects.create_user('kavatha', 'admin@gmail.com', 'django1234')
     client.login(username = userr.username, password='django1234')
+    return userr
 
-    note = Notes.objects.create(title='sample title', text='sample text', user=userr)
-    note = Notes.objects.create(title='another sample title', text='sample text', user=userr)
+@pytest.mark.django_db
+def test_list_endpoint_return_list(client, logged_user):
+    note = Notes.objects.create(title='sample title', text='sample text', user=logged_user)
+    note = Notes.objects.create(title='another sample title', text='sample text', user=logged_user)
     response = client.get(path='/smart/notes/')
     assert response.status_code == 200
     content = str(response.content)
@@ -20,18 +22,18 @@ def test_list_endpoint_return_list(client):
 
 
 @pytest.mark.django_db
-def test_list_endpoint_return_list_only_authenticatd_user(client):
+def test_list_endpoint_return_list_only_authenticatd_user(client, logged_user):
     # Create the first user and their notes
-    userr = User.objects.create_user('kavatha', 'admin@gmail.com', 'django1234')
+    # userr = User.objects.create_user('kavatha', 'admin@gmail.com', 'django1234')
     kav = User.objects.create_user('kav', 'admin2@gmail.com', 'django1234')
     Notes.objects.create(title='kav title', text='sample text', user=kav)
     
     # Log in as 'userr'
-    client.login(username=userr.username, password='django1234')
+    # client.login(username=logged_user.username, password='django1234')
 
     # Create notes for 'userr'
-    Notes.objects.create(title='sample title', text='sample text', user=userr)
-    Notes.objects.create(title='another sample title', text='sample text', user=userr)
+    Notes.objects.create(title='sample title', text='sample text', user=logged_user)
+    Notes.objects.create(title='another sample title', text='sample text', user=logged_user)
 
     # Get the response
     response = client.get(path='/smart/notes/')
